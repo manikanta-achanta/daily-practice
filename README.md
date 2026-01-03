c5b9756da0bd7bbd409e99164bdd6157866234fd
A.manikanta <manikantaachanta830@gmail.com>
2026-01-03 14:15:59 +0530

    Day 2: javascript variables practice

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # daily-GitHub-practice
 
 Day 1:learning how to make github contributions.
+
+## Day 2
+-learned javascript variables
---
+++ javascript/day2-variables.js
@@ -0,0 +1,7 @@
+// Day 2: JavaScript Variables
+
+let name = "Manikanta";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
